4c0d13bce3ad3507f5d1ab8ea9aa358f0e117441
7awaydidi <qataryababa@gmail.com>
2026-06-17 18:57:06 +0100

    Update bot connection details and log messages

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const mineflayer = require('mineflayer');
 const express = require('express');
 const app = express();
 
-// هنا بنفتح البورت اللي موقع Render جالس يبكي عليه
 const PORT = process.env.PORT || 10000;
 app.get('/', (req, res) => res.send('البوت شغال 100%!'));
 app.listen(PORT, () => console.log(`سيرفر الويب شغال على بورت ${PORT}`));
 
 function createBot() {
     const bot = mineflayer.createBot({
-        host: 'Slobos.aternos.me', // الأيبي حقك
-        port: 52633,               // البورت حقك
-        username: 'Slobos_Bot',
+        host: 'born-mc.aternos.me', 
+        port: 52633,               
+        username: 'Born_Bot',    
         version: '1.21.1'
     });
 
     bot.on('spawn', () => {
-        console.log('كفووو! البوت دخل السيرفر الحين وهو واقف AFK!');
+        console.log('كفووو! البوت دخل سيرفر BORN-MC الحين وهو واقف AFK!');
     });
 
     bot.on('error', (err) => {
